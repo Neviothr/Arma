834c60e9d095416f006f27a69d590eb53229a341
cx64 <cx64@users.noreply.github.com>
2016-05-22 16:28:32 +0300

    Create config.cpp

---
+++ CX-Addons/tracer/config.cpp
@@ -0,0 +1,94 @@
+class CfgPatches
+{
+	class tracers
+	{
+		units[]=
+		{
+			""
+		};
+		weapons[]=
+		{
+			""
+		};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"A3_Weapons_F",
+			"A3_Weapons_F_Beta",
+			"A3_Weapons_F_Gamma"
+		};
+	};
+};
+class CfgMagazines
+{
+	class CA_Magazine;
+	class VehicleMagazine;
+	class 30Rnd_65x39_caseless_mag: CA_Magazine
+	{
+		tracersEvery=1;
+	};
+	class 30Rnd_65x39_caseless_green: CA_Magazine
+	{
+		tracersEvery=1;
+	};
+	class 30Rnd_556x45_Stanag: CA_Magazine
+	{
+		tracersEvery=1;
+	};
+	class 100Rnd_65x39_caseless_mag: CA_Magazine
+	{
+		tracersEvery=1;
+	};
+	class 200Rnd_65x39_cased_Box: CA_Magazine
+	{
+		tracersEvery=1;
+	};
+	class 200Rnd_65x39_Belt: CA_Magazine
+	{
+		tracersEvery=1;
+	};
+	class 150Rnd_93x64_Mag: CA_Magazine
+	{
+		tracersEvery=1;
+	};
+	class 130Rnd_338_Mag: CA_Magazine
+	{
+		tracersEvery=1;
+	};
+	class 30Rnd_45ACP_Mag_SMG_01: CA_Magazine
+	{
+		tracersEvery=1;
+	};
+	class 30Rnd_9x21_Mag: CA_Magazine
+	{
+		tracersEvery=1;
+	};
+	class 10Rnd_127x54_Mag: CA_Magazine
+	{
+		tracersEvery=1;
+	};
+	class 10Rnd_93x64_DMR_05_Mag: CA_Magazine
+	{
+		tracersEvery=1;
+	};
+	class 5Rnd_127x108_Mag: CA_Magazine
+	{
+		tracersEvery=1;
+	};
+	class 7Rnd_408_Mag: CA_Magazine
+	{
+		tracersEvery=1;
+	};
+	class 10Rnd_338_Mag: CA_Magazine
+	{
+		tracersEvery=1;
+	};
+	class 20Rnd_762x51_Mag: CA_Magazine
+	{
+		tracersEvery=1;
+	};
+	class 20Rnd_556x45_UW_mag: CA_Magazine
+	{
+		tracersEvery=1;
+	};
+};
