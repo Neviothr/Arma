999dd4132889c11c59be1e1355e7c15cf03a1188
cx64 <cx64@users.noreply.github.com>
2016-07-24 15:00:19 +0300

    Create config.cpp

---
+++ CX-Addons/cx_nvgs/config.cpp
@@ -0,0 +1,26 @@
+class CfgPatches
+{
+	class cx_nvg
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=1.0;
+		requiredAddons[]={"A3_Weapons_F"};
+	};
+};
+
+class CfgWeapons
+{
+	class Binocular;
+	class NVGoggles
+	{
+		displayName = "Custom NVGs";
+		modelOptics="\cx_nvg\lens";
+		Scope=2;
+		ScopeCurator=2;
+		class ItemInfo
+		{
+			mass = 1;
+		};
+	};
+};
