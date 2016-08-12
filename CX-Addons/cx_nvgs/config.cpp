4077c15897bb42363539351fce78b1cab3a76339
cx64 <cx64@users.noreply.github.com>
2016-08-12 17:22:16 +0300

    Fixed.

--- CX-Addons/cx_nvgs/config.cpp
+++ CX-Addons/cx_nvgs/config.cpp
@@ -1,26 +1,47 @@
 class CfgPatches
 {
-	class cx_nvg
-	{
-		units[]={};
-		weapons[]={};
-		requiredVersion=1.0;
-		requiredAddons[]={"A3_Weapons_F"};
-	};
+    class cx_nvg
+    {
+        units[]={};
+        weapons[]={};
+        requiredVersion=1.0;
+        requiredAddons[]={"A3_Weapons_F"};
+    };
 };
 
 class CfgWeapons
 {
-	class Binocular;
-	class NVGoggles
-	{
-		displayName = "Custom NVGs";
-		modelOptics="\cx_nvg\twotube";
-		Scope=2;
-		ScopeCurator=2;
-		class ItemInfo
-		{
-			mass = 1;
-		};
-	};
+    class Binocular;
+    class NVGoggles
+    {
+        class ItemInfo;
+    };
+
+    class cx_pvs14: NVGoggles
+    {
+        author = "cx64";
+        modelOptics = "\cx_nvg\onetube";
+        displayName = "PVS-14";
+        scope = 2;
+        scopeCurator = 2;
+
+        class ItemInfo: ItemInfo
+        {
+            mass = 1;
+        };
+    };
+
+    class cx_pvs15: cx_pvs14
+    {
+        author = "cx64";
+        modelOptics = "\cx_nvg\twotube";
+        displayName = "PVS-15";
+        scope = 2;
+        scopeCurator = 2;
+
+        class ItemInfo: ItemInfo
+        {
+            mass = 1;
+        };
+    };
 };
