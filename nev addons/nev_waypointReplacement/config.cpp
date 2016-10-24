ae49a6f567eaef6eb6399af44e6567b9be728123
Neviothr <Neviothr@users.noreply.github.com>
2016-10-24 19:18:24 +0300

    Returned CBA dependency

--- nev addons/nev_waypointReplacement/config.cpp
+++ nev addons/nev_waypointReplacement/config.cpp
@@ -2,27 +2,16 @@ class CfgPatches
 {
 	class nev_waypointReplacement
 	{
-		name = "Personal Waypoint Replacement";
-		author = "Neviothr";
-		units[] = {};
-		weapons[] = {};
-		requiredVersion = 1.0;
-		requiredAddons[] = {"A3_Weapons_F"};
-
+ 		name = "Personal Waypoint Replacement";
+ 		author = "Neviothr";
+  		units[] = {};
+  		weapons[] = {};
+  		requiredVersion = 1.0;
+		requiredAddons[] = {"Extended_EventHandlers"};
 	};
 };
 
-class CfgFunctions
+class Extended_PostInit_EventHandlers
 {
-	class nev
-	{
-		class waypoint
-		{
-			class replacement_init
-			{
-				file = "nev_waypointReplacement\waypointDistance.sqf";
-				preInit = 1;
-			};
-		};
-	};
+		nev_waypointReplacement_Post_Init = "nev_waypointReplacement_Post_Init_Var = [] execVM ""\nev_waypointReplacement\waypointDistance.sqf""";
 };
