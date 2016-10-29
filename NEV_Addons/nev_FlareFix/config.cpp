1c2bad92fec267b7379c0255ab9d945234683893
Neviothr <Neviothr@users.noreply.github.com>
2016-10-29 10:09:00 +0300

    Increased flare up time, made flares fall slower

--- NEV_Addons/nev_FlareFix/config.cpp
+++ NEV_Addons/nev_FlareFix/config.cpp
@@ -16,26 +16,30 @@ class CfgAmmo
 	class FlareBase;
 	class Flare_82mm_AMOS_White: FlareCore
 	{
-		timeToLive = 35;
+		timeToLive = 40;
 		brightness = 4000;
 		intensity  = 5000000;
+		coefGravity = 0.25;
 	};
 	class F_40mm_White: FlareBase
 	{
-		timeToLive = 35;
+		timeToLive = 40;
 		brightness = 4000;
 		intensity  = 5000000;
+		coefGravity = 0.25;
 	};
 	class F_20mm_White: FlareBase
 	{
-		timeToLive = 35;
+		timeToLive = 40;
 		brightness = 4000; // 8 (1250:1)
 		intensity  = 5000000; // 10000
+		coefGravity = 0.25;
 	};
 	class F_Signal_Green: FlareBase
 	{
-		timeToLive = 35;
+		timeToLive = 40;
 		brightness = 4000;
 		intensity  = 5000000;
+		coefGravity = 0.25;
 	};
 };
