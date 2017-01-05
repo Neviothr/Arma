bb04fbd98cfed29b03b226969d6a6801a036c39e
Neviothr <Neviothr@users.noreply.github.com>
2017-01-05 12:40:23 +0200

    Add files via upload

---
+++ ACE2/ace_m_wep_rpg7/config.cpp
@@ -0,0 +1,19 @@
+class CfgPatches
+{
+	class ace_m_wep_rpg7
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=1.59;
+		requiredAddons[]=
+		{
+			"ace_main"
+		};
+		version="1.12.1.510";
+		authors[]=
+		{
+			"Solus",
+			"zGuba"
+		};
+	};
+};
