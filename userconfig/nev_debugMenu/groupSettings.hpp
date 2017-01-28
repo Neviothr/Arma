df2cc099d78cb6e17ff821b47f47803a89ce5dea
Neviothr <Neviothr@users.noreply.github.com>
2017-01-28 18:19:50 +0200

    Add files via upload

---
+++ userconfig/nev_debugMenu/groupSettings.hpp
@@ -0,0 +1,5 @@
+// Side of spawned units (west, east, independent, civ)
+NEV_groupSide = west;
+
+// Array of units to spawn as a group
+NEV_group = ["B_CTRG_Soldier_Medic_tna_F","B_CTRG_Soldier_JTAC_tna_F","B_CTRG_Soldier_TL_tna_F","B_CTRG_Soldier_AR_tna_F","B_CTRG_Soldier_tna_F","B_CTRG_Soldier_LAT_tna_F"];
