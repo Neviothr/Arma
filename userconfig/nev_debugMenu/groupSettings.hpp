6606d01cc312eefb1533fe59f29198d14f2c2150
Neviothr <Neviothr@users.noreply.github.com>
2017-02-07 20:20:39 +0200

    Add files via upload

--- userconfig/nev_debugMenu/groupSettings.hpp
+++ userconfig/nev_debugMenu/groupSettings.hpp
@@ -5,8 +5,8 @@ NEV_groupSide = east;
 NEV_group = ["rhs_msv_at", "rhs_msv_machinegunner", "rhs_msv_rifleman", "rhs_msv_grenadier", "rhs_msv_sergeant"];
 
 // The max distance from the player groups will be spawned at
-NEV_maxDistance = 325
+NEV_maxDistance = 325; // Default is 325
 
 // NOTE: The max distance must be higher than the min distance
 // The min distance from the player groups will be spawn at
-NEV_minDistance = 250
+NEV_minDistance = 250; // Default is 250
