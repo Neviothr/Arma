73a34ed87f916c10c9571d7bb0b9b55264097cfa
Neviothr <Neviothr@users.noreply.github.com>
2017-02-27 21:44:13 +0200

    Add files via upload

--- NEV_Addons/mod.cpp
+++ NEV_Addons/mod.cpp
@@ -1,1 +1,11 @@
 name = "Neviothr's Mod Pack";
+picture = "\nev_debugMenu\logo.paa";
+logoSmall = "\nev_debugMenu\logo.paa";
+logo = "\nev_debugMenu\logo.paa";
+logoOver = "\nev_debugMenu\logo.paa";
+action = "https://github.com/Neviothr/ArmA";
+tooltipOwned = "Neviothr's addons";
+dlcColor[] = {1, 0.706, 0.075, 1};
+overview = "Neviothr's addons. A collection of addons designed to improve gameplay.";
+hideName = 0
+hidePicture = 0;
