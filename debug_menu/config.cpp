7b7191be88aefcedb316d5d150c0c4f60851fe9e
Neviothr <Neviothr@users.noreply.github.com>
2017-03-14 17:24:19 +0200

    Includes don't want to work.

---
+++ debug_menu/config.cpp
@@ -0,0 +1,14 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+      class ADDON {
+            units[] = {};
+            weapons[] = {};
+            requiredVersion = REQUIRED_VERSION;
+            author = "Neviothr"
+            authors[] = {"Neviothr"};
+            PBO_URL;
+            VERSION_CONFIG;
+            requiredAddons[] = {"3den"};
+      };
+};
