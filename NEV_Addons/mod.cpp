03b22984d6c5cc086b8560a9f69b0bf1da22e100
Neviothr <zaharkokin@gmail.com>
2017-03-15 12:44:04 +0200

    Changed description.
    It now reflects more accurately the mod(s).

--- NEV_Addons/mod.cpp
+++ NEV_Addons/mod.cpp
@@ -1,11 +1,11 @@
 name = "Neviothr's Mod Pack";
-picture = "\nev_debugMenu\logo.paa";
-logoSmall = "\nev_debugMenu\logo.paa";
-logo = "\nev_debugMenu\logo.paa";
-logoOver = "\nev_debugMenu\logo.paa";
+picture = "\debug_menu\data\logo.paa";
+logoSmall = "\debug_menu\data\logo.paa";
+logo = "\debug_menu\data\logo.paa";
+logoOver = "\debug_menu\data\logo.paa";
 action = "https://github.com/Neviothr/ArmA";
-tooltipOwned = "Neviothr's addons";
+tooltipOwned = "NEV Addons";
 dlcColor[] = {1, 0.706, 0.075, 1};
-overview = "Neviothr's addons. A collection of addons designed to improve gameplay.";
+overview = "Neviothr's addons. A collection of addons designed to improve gameplay. Contains standalone, vanilla, and mod overriding addons.";
 hideName = 0
 hidePicture = 0;
