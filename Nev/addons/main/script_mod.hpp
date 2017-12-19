b99ef7b27071c5e7329fa2f3964e410d3b74f8a5
Neviothr <zaharkokin@gmail.com>
2017-12-19 19:11:52 +0200

    Increment version. gear_ca.paa didn't commit?

--- Nev/addons/main/script_mod.hpp
+++ Nev/addons/main/script_mod.hpp
@@ -9,7 +9,7 @@
 	PATCH version when a release contains only bug fixes
 */
 #define MAJOR 0
-#define MINOR 4
+#define MINOR 5
 #define PATCHLVL 0
 #define BUILD 171218 // Use the DATE for the BUILD in the format YYMMDD
 
