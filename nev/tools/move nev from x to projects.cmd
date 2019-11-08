308b5e8bc381befbae2001f07d2ee0855c5afc93
Neviothr <Neviothr@users.noreply.github.com>
2019-11-08 19:14:57 +0200

    * minor upate to tools

--- nev/tools/move nev from x to projects.cmd
+++ nev/tools/move nev from x to projects.cmd
@@ -1,14 +1,15 @@
+@echo off
 rd "E:\GitHub Projects\Arma\nev\addons" /s /q
 mkdir "E:\GitHub Projects\Arma\nev\addons"
-xcopy "E:\x\nev\addons" "E:\GitHub Projects\Arma\nev\addons" /e
+xcopy "E:\x\nev" "E:\GitHub Projects\Arma\nev\" /e
 
 :choice
-@echo off
-set /P c=Open GitHub Desktop[Y/N]?
+set /P c=Open GitHub Desktop [Enter / N]?
 @echo on
 if /I "%c%" EQU "Y" goto :open_github
 if /I "%c%" EQU "N" goto :exit
 
+@echo off
 :open_github
 start C:\Users\Admin\AppData\Local\GitHubDesktop\GitHubDesktop.exe
 
