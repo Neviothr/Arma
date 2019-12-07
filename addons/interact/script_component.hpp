3a85820fb77e88ff3faa82281b0829611c02b04e
Neviothr <Neviothr@users.noreply.github.com>
2019-12-07 22:34:22 +0200

    remove unflip actions from interaction, are present in ACE; use tele func for debug

--- addons/interact/script_component.hpp
+++ addons/interact/script_component.hpp
@@ -3,15 +3,3 @@
 #include "\x\nev\addons\main\script_mod.hpp"
 
 #include "\x\nev\addons\main\script_macros.hpp"
-
-#define UNFLIP_ACTION \
-    class ACE_Actions { \
-        class ACE_MainActions { \
-            class GVAR(unflip) { \
-                displayName = "Unflip Vehicle"; \
-                condition = "1 < (vectorUp _target) vectorDistance (surfaceNormal getPosATL _target)"; \
-                statement = "private _position = getPosATL _target; _target setVectorUp surfaceNormal _position; _target setPosATL _position;"; \
-                exceptions[] = {"isNotSwimming"}; \
-            }; \
-        }; \
-    }
