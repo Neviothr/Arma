0348a12d53c1173c00927b9bacb689fd4c21c1b6
Neviothr <Neviothr@users.noreply.github.com>
2020-01-10 21:59:18 +0200

    Add vehicle weapons to ai_rof; update CfgWeaponsReference with CUP units and vehicles

--- addons/ai_rof/CfgWeapons.hpp
+++ addons/ai_rof/CfgWeapons.hpp
@@ -20,6 +20,16 @@ class CfgWeapons {
     class CUP_arifle_xm8_sharpshooter;
     class CUP_arifle_AKS_Base;
     class CUP_arifle_AK_Base;
+    class MGun;
+    class MGunCore;
+    class GMG_F;
+    class CannonCore;
+    class HMG_M2;
+    class gatling_30mm;
+    class GMG_20mm;
+    class autocannon_30mm_CTWS;
+    class rhs_weap_m134_minigun_1;
+    class HMG_127;
 
     // Manually add the "NEV_FullAuto" firemode to modes[] array because += operator doesn't work reliably.
     class DMR_01_base_F: Rifle_Long_Base_F {
@@ -517,5 +527,275 @@ class CfgWeapons {
     class CUP_arifle_SCAR_Base: Rifle_Base_F {
         modes[] = {"SCAR_L_Single", "SCAR_L_FullLAuto", "NEV_FullAuto"};
         AI_ROF(SCAR_L_FullLAuto);
+    };
+
+    // Vehicle weapons
+    class LMG_RCWS: MGun {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class M134_minigun: MGunCore {
+        modes[] = {"HighROF", "LowROF", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(HighROF);
+    };
+    class GMG_20mm: GMG_F {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class GMG_40mm: GMG_F {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class gatling_20mm: CannonCore {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class gatling_30mm_base: CannonCore {
+        modes[] = {"LowROF", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(LowROF);
+    };
+    class gatling_25mm: CannonCore {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class autocannon_35mm: CannonCore {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class Gatling_30mm_Plane_CAS_01_F: CannonCore {
+        modes[] = {"LowROF", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(LowROF);
+    };
+    class Cannon_30mm_Plane_CAS_02_F: CannonCore {
+        modes[] = {"LowROF", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(LowROF);
+    };
+    class M61_Vulcan_CIWS: Gatling_30mm_Plane_CAS_01_F {
+        modes[] = {"LowROF", "AI", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(LowROF);
+    };
+    class CUP_Vhmg_DSHKM_veh: MGun {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vhmg_M2_veh: HMG_M2 {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vhmg_M3P_veh: CUP_Vhmg_M2_veh {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vlmg_M240_veh: MGun {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vlmg_UK59_veh: MGun {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vlmg_M134_veh: MGunCore {
+        modes[] = {"LowROF", "HighROF", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(HighROF);
+    };
+    class CUP_Vhmg_KORD_veh: MGun {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vlmg_MG3_veh: MGun {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vhmg_PKT_veh: MGun {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vhmg_KPVT_veh: MGun {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vhmg_GAU19_veh: MGun {
+        modes[] = {"LowROF", "HighROF", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(HighROF);
+    };
+    class CUP_Vacannon_M197_veh: CannonCore {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vacannon_M242_veh: CannonCore {
+        modes[] = {"LowROF", "HighROF", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(HighROF);
+    };
+    class CUP_Vacannon_M230_veh: CannonCore {
+        modes[] = {"close", "short", "medium", "far", "burst_5", "burst_10", "burst_15", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(burst_15);
+    };
+    class CUP_Vacannon_2A14_veh: CannonCore {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vacannon_AZP23_veh: CannonCore {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vacannon_2A42_veh: CannonCore {
+        modes[] = {"LowROF", "HighROF", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(HighROF);
+    };
+    class CUP_Vacannon_2A72_veh: CUP_Vacannon_2A42_veh {
+        modes[] = {"LowROF", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(LowROF);
+    };
+    class CUP_Vacannon_M621_AW159_veh: CannonCore {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vacannon_GI2_veh: CannonCore {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vacannon_GSh23L_in_veh: CannonCore {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vacannon_GAU8_veh: CannonCore {
+        modes[] = {"halfsec", "1sec", "2sec", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(2sec);
+    };
+    class CUP_Vacannon_GAU12_veh: CannonCore {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vacannon_GAU22_veh: CannonCore {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vacannon_M168_M163VADS: CannonCore {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class CUP_Vacannon_GSh302K_veh: CannonCore {
+        modes[] = {"LowROF", "close", "near", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(LowROF);
+    };
+    class CUP_Vcannon_L30A1_veh: CannonCore {
+        modes[] = {"player", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(player);
+    };
+    class CUP_Vacannon_GSh23L_pod_veh: CannonCore {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class MMG_02_vehicle: LMG_RCWS {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class weapon_Cannon_Phalanx: CannonCore {
+        modes[] = {"manual", "close", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class weapon_Fighter_Gun20mm_AA: CannonCore {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class weapon_Fighter_Gun_30mm: CannonCore {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class MMG_01_vehicle: LMG_RCWS {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class rhs_weap_DSHKM: LMG_RCWS {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class rhs_weap_kpvt: MGun {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class rhs_weap_yakB: gatling_30mm {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class RHS_weap_Ags30: GMG_20mm {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class rhs_weap_pkm_tigr: rhs_weap_pkt {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class rhs_weap_2a42_base: autocannon_30mm_CTWS {
+        modes[] = {"LowROFBMD2", "HighROFBMD2", "closeBMD2", "shortBMD2", "mediumBMD2", "farBMD2", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(HighROFBMD2);
+    };
+    class rhs_weap_gsh30: gatling_30mm {
+        modes[] = {"manual", "manual_low", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class rhs_weap_gsh302: rhs_weap_gsh30 {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class rhs_weap_gsh301: rhs_weap_gsh30 {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class rhs_weap_gsh23l: rhs_weap_gsh30 {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class rhs_weap_azp23: CannonCore {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class RHS_weap_m134_pylon: rhs_weap_m134_minigun_1 {
+        modes[] = {"LowROF", "HighROF", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(HighROF);
+    };
+    class rhs_weap_M197: gatling_30mm {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class rhs_weap_M301: rhs_weap_M197 {
+        modes[] = {"manual", "HighROF", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class rhs_weap_M230: rhs_weap_M197 {
+        modes[] = {"manual", "burst_20", "burst_50", "burst_100", "burst_all", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(burst_all);
+    };
+    class RHS_weap_gau8: Gatling_30mm_Plane_CAS_01_F {
+        modes[] = {"LowROF", "HighROF", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(HighROF);
+    };
+    class RHS_weap_gau19: HMG_127 {
+        modes[] = {"LowROF", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(LowROF);
+    };
+    class RHS_weap_zpl20: gatling_30mm {
+        modes[] = {"manual", "manual_low", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class LMG_65mm_body: LMG_RCWS {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class LMG_M200: LMG_RCWS {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class LMG_M200_body: LMG_M200 {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class LMG_Minigun: LMG_RCWS {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
+    };
+    class LMG_Minigun_heli: LMG_Minigun {
+        modes[] = {"manual", "close", "short", "medium", "far", "NEV_FullAuto"};
+        AI_VEHICLE_ROF(manual);
     };
 };
--- addons/ai_rof/script_component.hpp
+++ addons/ai_rof/script_component.hpp
@@ -19,8 +19,29 @@
             burstRangeMax = 10; \
             showToPlayer = 0; \
             aiDispersionCoefX = 30.0; \
-            aiDispersionCoefY = 30.0 ;\
+            aiDispersionCoefY = 30.0; \
             aiRateOfFireDispersion = 5; \
             aiBurstTerminable = false; \
             dispersion = 0.004; \
         }
+
+# define AI_VEHICLE_ROF(baseMode) \
+        class baseMode; \
+        class NEV_FullAuto: ##baseMode { \
+            aiRateOfFire = 0.1; \
+            aiRateOfFireDistance = 5000; \
+            minRange = 0; \
+            minRangeProbab = 1; \
+            midRange = 500; \
+            midRangeProbab = 1; \
+            maxRange = 1000; \
+            maxRangeProbab = 1; \
+            burst = 25; \
+            burstRangeMax = 50; \
+            showToPlayer = 0; \
+            aiRateOfFireDispersion = 5; \
+            aiBurstTerminable = false; \
+            dispersion = 0.004; \
+        }; \
+        aiDispersionCoefX = 30.0; \
+        aiDispersionCoefY = 30.0
