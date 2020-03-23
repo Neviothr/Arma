ae3a0c5ec1e1e144b09352456f6c7e0eb34e26b1
Neviothr <Neviothr@users.noreply.github.com>
2020-03-23 19:48:46 +0200

    Clean up

--- tools/build.cmd
+++ tools/build.cmd
@@ -9,13 +9,13 @@ for /d %%f in (E:\x\nev\addons\*) do (
     set sourceFolder=%%f
     set pboName=!sourceFolder:E:\x\nev\addons\=!
     set targetPath="E:\Steam\SteamApps\common\Arma 3\@nev\addons\nev_!pboName!.pbo"
-    
+
     echo Building - !pboName!
     !armakeEXE! build -v -f -w "unquoted-string" -i "E:\x" "!sourceFolder!" !targetPath!
-    
+
     echo Signing - !pboName!
     !armakeEXE! sign -v -f !privateKey! !targetPath!
-    
+
     echo.
 )
 
