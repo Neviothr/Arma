079698919c91f625c0cc8209def6953d3db5510a
Neviothr <Neviothr@users.noreply.github.com>
2020-03-29 14:14:58 +0300

    * enable debug_mode_full for all components to allow usage of LOG() and TRACE_n() macros

--- addons/brighter_flares/script_component.hpp
+++ addons/brighter_flares/script_component.hpp
@@ -2,6 +2,8 @@
 
 #include "\x\nev\addons\main\script_mod.hpp"
 
+#define DEBUG_MODE_FULL
+
 #include "\x\nev\addons\main\script_macros.hpp"
 
 #define BRIGHTER_FLARE \
--- addons/mission_framework/script_component.hpp
+++ addons/mission_framework/script_component.hpp
@@ -2,4 +2,6 @@
 
 #include "\x\nev\addons\main\script_mod.hpp"
 
+#define DEBUG_MODE_FULL
+
 #include "\x\nev\addons\main\script_macros.hpp"
