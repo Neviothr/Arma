267bd82f531ddbd5153173f0a6848eb4904ddd59
Neviothr <Neviothr@users.noreply.github.com>
2020-04-04 16:30:59 +0300

    Update cup_weapons_ace_comat

--- addons/cup_weapons_ace_compat/CfgMagazines.hpp
+++ addons/cup_weapons_ace_compat/CfgMagazines.hpp
@@ -1,134 +1,122 @@
-class CfgMagazines {
-    /* **************************************************************************
-     * 			ACE Explosives
-     * ************************************************************************** */
-    class CA_Magazine;
-    class CUP_TimeBomb_M: CA_Magazine {
-        scope = 1;
-        ace_explosives_placeable = 1; // Can be placed
-        useAction = 0; // Disable the vanilla interaction
-        ace_explosives_SetupObject = "ACE_PipeBomb_place_CUP"; // The object placed before the explosive is armed
+class CfgMagazines
+{
+	class CA_LauncherMagazine;
+	class CUP_NLAW_M: CA_LauncherMagazine
+	{
+		ammo = "ACE_NLAW";
+	};
 
-        class ACE_Triggers { // Trigger configurations
-            SupportedTriggers[] = {"Timer", "Command", "MK16_Transmitter", "DeadmanSwitch", "Cellphone"}; // Triggers that can be used
-            class Timer {
-                FuseTime = 0.5; // Time for the fuse to burn
-            };
+	/* **************************************************************************
+	 * 			ACE Explosives
+	 * ************************************************************************** */
+	class CA_Magazine;
+	class CUP_TimeBomb_M: CA_Magazine
+	{
+		scope = 1;
+		ace_explosives_placeable = 1;  // Can be placed
+		useAction = 0;  // Disable the vanilla interaction
+		ace_explosives_SetupObject = "ACE_PipeBomb_place_CUP";  // The object placed before the explosive is armed
+		class ACE_Triggers {  // Trigger configurations
+			SupportedTriggers[] = {"Timer", "Command", "MK16_Transmitter", "DeadmanSwitch", "Cellphone"};  // Triggers that can be used
+			class Timer {
+				FuseTime = 0.5;  // Time for the fuse to burn
+			};
+			class Command {
+				FuseTime = 0.5;
+			};
+			class MK16_Transmitter: Command {};
+			class DeadmanSwitch: Command {};
+		};
+	};
+	class CUP_Mine_M: CUP_TimeBomb_M
+	{
+		ace_explosives_placeable = 1;  // Can be placed
+		useAction = 0;  // Disable the vanilla interaction
+		ace_explosives_SetupObject = "ACE_Mine_place_CUP";  // The object placed before the explosive is armed
+		class ACE_Triggers {  // Trigger configurations
+			SupportedTriggers[] = {"PressurePlate"};  // Triggers that can be used
+			class PressurePlate {
+				digDistance = 0.08;
+			};
+		};
+	};
+	class CUP_MineE_M: CUP_TimeBomb_M
+	{
+		ace_explosives_placeable = 1;  // Can be placed
+		useAction = 0;  // Disable the vanilla interaction
+		ace_explosives_SetupObject = "ACE_MineE_place_CUP";  // The object placed before the explosive is armed
+		class ACE_Triggers {  // Trigger configurations
+			SupportedTriggers[] = {"PressurePlate"};  // Triggers that can be used
+			class PressurePlate {
+				digDistance = 0.06;
+			};
+		};
+	};
 
-            class Command {
-                FuseTime = 0.5;
-            };
-
-            class MK16_Transmitter: Command {};
-            class DeadmanSwitch: Command {};
-        };
-    };
-
-    class CUP_Mine_M: CUP_TimeBomb_M {
-        ace_explosives_placeable = 1; // Can be placed
-        useAction = 0; // Disable the vanilla interaction
-        ace_explosives_SetupObject = "ACE_Mine_place_CUP"; // The object placed before the explosive is armed
-
-        class ACE_Triggers { // Trigger configurations
-            SupportedTriggers[] = {"PressurePlate"}; // Triggers that can be used
-
-            class PressurePlate {
-                digDistance = 0.08;
-            };
-        };
-    };
-
-    class CUP_MineE_M: CUP_TimeBomb_M {
-        ace_explosives_placeable = 1; // Can be placed
-        useAction = 0; // Disable the vanilla interaction
-        ace_explosives_SetupObject = "ACE_MineE_place_CUP"; // The object placed before the explosive is armed
-    
-        class ACE_Triggers { // Trigger configurations
-            SupportedTriggers[] = {"PressurePlate"}; // Triggers that can be used
-            class PressurePlate {
-                digDistance = 0.06;
-            };
-        };
-    };
-
-    class CUP_IED_V1_M: CUP_Mine_M {
-        ace_explosives_placeable = 1; // Can be placed
-        useAction = 0; // Disable the vanilla interaction
-        ace_explosives_SetupObject = "ACE_IED_V1_place_CUP"; // The object placed before the explosive is armed
-        
-        class ACE_Triggers { // Trigger configurations
-            SupportedTriggers[] = {"Timer", "Command", "MK16_Transmitter", "DeadmanSwitch", "Cellphone", "PressurePlate"}; // Triggers that can be used
-        };
-    };
-    
-    class CUP_IED_V2_M: CUP_IED_V1_M {
-        useAction = 0;
-        ace_explosives_SetupObject = "ACE_IED_V2_place_CUP";
-    };
-    
-    class CUP_IED_V3_M: CUP_IED_V1_M {
-        useAction = 0;
-        ace_explosives_SetupObject = "ACE_IED_V3_place_CUP";
-        
-        class ACE_Triggers { // Trigger configurations
-            SupportedTriggers[] = {"Timer", "Command", "MK16_Transmitter", "DeadmanSwitch", "Cellphone", "PressurePlate"}; // Triggers that can be used
-            
-            class Timer {
-                digDistance = 0.06;
-            };
-            
-            class Command {
-                digDistance = 0.06;
-            };
-            
-            class MK16_Transmitter {
-                digDistance = 0.06;
-            };
-            
-            class DeadmanSwitch {
-                digDistance = 0.06;
-            };
-            
-            class Cellphone {
-                digDistance = 0.06;
-            };
-            
-            class PressurePlate {
-                digDistance = 0.06;
-            };
-        };
-    };
-    
-    class CUP_IED_V4_M: CUP_IED_V1_M {
-        useAction = 0;
-        ace_explosives_SetupObject = "ACE_IED_V4_place_CUP";
-        
-        class ACE_Triggers { // Trigger configurations
-            SupportedTriggers[] = {"Timer", "Command", "MK16_Transmitter", "DeadmanSwitch", "Cellphone", "PressurePlate"}; // Triggers that can be used
-            
-            class Timer {
-                digDistance = 0.08;
-            };
-            
-            class Command {
-                digDistance = 0.08;
-            };
-            
-            class MK16_Transmitter {
-                digDistance = 0.08;
-            };
-            
-            class DeadmanSwitch {
-                digDistance = 0.08;
-            };
-            
-            class Cellphone {
-                digDistance = 0.08;
-            };
-            
-            class PressurePlate {
-                digDistance = 0.08;
-            };
-        };
-    };
+	class CUP_IED_V1_M: CUP_Mine_M
+	{
+		ace_explosives_placeable = 1;  // Can be placed
+		useAction = 0;  // Disable the vanilla interaction
+		ace_explosives_SetupObject = "ACE_IED_V1_place_CUP";  // The object placed before the explosive is armed
+		class ACE_Triggers {  // Trigger configurations
+			SupportedTriggers[] = {"Timer", "Command", "MK16_Transmitter", "DeadmanSwitch", "Cellphone", "PressurePlate"};  // Triggers that can be used
+		};
+	};
+	class CUP_IED_V2_M: CUP_IED_V1_M
+	{
+		useAction = 0;
+		ace_explosives_SetupObject = "ACE_IED_V2_place_CUP";
+	};
+	class CUP_IED_V3_M: CUP_IED_V1_M
+	{
+		useAction = 0;
+		ace_explosives_SetupObject = "ACE_IED_V3_place_CUP";
+		class ACE_Triggers {  // Trigger configurations
+			SupportedTriggers[] = {"Timer", "Command", "MK16_Transmitter", "DeadmanSwitch", "Cellphone", "PressurePlate"};  // Triggers that can be used
+			class Timer {
+				digDistance = 0.06;
+			};
+			class Command {
+				digDistance = 0.06;
+			};
+			class MK16_Transmitter {
+				digDistance = 0.06;
+			};
+			class DeadmanSwitch {
+				digDistance = 0.06;
+			};
+			class Cellphone {
+				digDistance = 0.06;
+			};
+			class PressurePlate {
+				digDistance = 0.06;
+			};
+		};
+	};
+	class CUP_IED_V4_M: CUP_IED_V1_M
+	{
+		useAction = 0;
+		ace_explosives_SetupObject = "ACE_IED_V4_place_CUP";
+		class ACE_Triggers {  // Trigger configurations
+			SupportedTriggers[] = {"Timer", "Command", "MK16_Transmitter", "DeadmanSwitch", "Cellphone", "PressurePlate"};  // Triggers that can be used
+			class Timer {
+				digDistance = 0.08;
+			};
+			class Command {
+				digDistance = 0.08;
+			};
+			class MK16_Transmitter {
+				digDistance = 0.08;
+			};
+			class DeadmanSwitch {
+				digDistance = 0.08;
+			};
+			class Cellphone {
+				digDistance = 0.08;
+			};
+			class PressurePlate {
+				digDistance = 0.08;
+			};
+		};
+	};
 };
--- addons/cup_weapons_ace_compat/CfgVehicles.hpp
+++ addons/cup_weapons_ace_compat/CfgVehicles.hpp
@@ -1,38 +1,32 @@
 class CfgVehicles {
-    class ACE_Explosives_Place;
-    class ACE_PipeBomb_place_CUP: ACE_Explosives_Place {
-        displayName = "Satchel Charge"; // Name of the item
-        model = "\CUP\Weapons\CUP_Weapons_Put\CUP_Satchel.p3d"; // Path to your model
-        ace_explosives_offset[] = {0, 0, 0}; // Offset of the interaction point from the model in meters on the X,Y,Z axis.
-    };
-
-    class ACE_Mine_place_CUP: ACE_Explosives_Place {
-        displayName = "AT-15 Anti-Tank Mine"; // Name of the item
-        model = "\CUP\Weapons\CUP_Weapons_Put\CUP_AT15.p3d"; // Path to your model
-        ace_explosives_offset[] = {0, 0, 0}; // Offset of the interaction point from the model in meters on the X,Y,Z axis.
-    };
-
-    class ACE_MineE_place_CUP: ACE_Explosives_Place {
-        displayName = "TM46 Anti-Tank Mine"; // Name of the item
-        model = "\CUP\Weapons\CUP_Weapons_Put\CUP_TM46.p3d"; // Path to your model
-        ace_explosives_offset[] = {0, 0, 0}; // Offset of the interaction point from the model in meters on the X,Y,Z axis.
-    };
-
-    class ACE_IED_V1_place_CUP: ACE_Explosives_Place {
-        displayName = "IED"; // Name of the item
-        model = "\CUP\Weapons\CUP_Weapons_Put\CUP_IED_V1.p3d"; // Path to your model
-        ace_explosives_offset[] = {0, 0, 0}; // Offset of the interaction point from the model in meters on the X,Y,Z axis.
-    };
-
-    class ACE_IED_V2_place_CUP: ACE_IED_V1_place_CUP {
-        model = "\CUP\Weapons\CUP_Weapons_Put\CUP_IED_V2.p3d";
-    };
-
-    class ACE_IED_V3_place_CUP: ACE_IED_V1_place_CUP {
-        model = "\CUP\Weapons\CUP_Weapons_Put\CUP_IED_V3.p3d";
-    };
-
-    class ACE_IED_V4_place_CUP: ACE_IED_V1_place_CUP {
-        model = "\CUP\Weapons\CUP_Weapons_Put\CUP_IED_V4.p3d";
-    };
+	class ACE_Explosives_Place;
+	class ACE_PipeBomb_place_CUP: ACE_Explosives_Place {
+		displayName = "Satchel Charge";  // Name of the item
+		model = "\CUP\Weapons\CUP_Weapons_Put\CUP_Satchel.p3d";  // Path to your model
+		ace_explosives_offset[] = {0, 0, 0};  // Offset of the interaction point from the model in meters on the X,Y,Z axis.
+	};
+	class ACE_Mine_place_CUP: ACE_Explosives_Place {
+		displayName = "AT-15 Anti-Tank Mine";  // Name of the item
+		model = "\CUP\Weapons\CUP_Weapons_Put\CUP_AT15.p3d";  // Path to your model
+		ace_explosives_offset[] = {0, 0, 0};  // Offset of the interaction point from the model in meters on the X,Y,Z axis.
+	};
+	class ACE_MineE_place_CUP: ACE_Explosives_Place {
+		displayName = "TM46 Anti-Tank Mine";  // Name of the item
+		model = "\CUP\Weapons\CUP_Weapons_Put\CUP_TM46.p3d";  // Path to your model
+		ace_explosives_offset[] = {0, 0, 0};  // Offset of the interaction point from the model in meters on the X,Y,Z axis.
+	};
+	class ACE_IED_V1_place_CUP: ACE_Explosives_Place {
+		displayName = "IED";  // Name of the item
+		model = "\CUP\Weapons\CUP_Weapons_Put\CUP_IED_V1.p3d";  // Path to your model
+		ace_explosives_offset[] = {0, 0, 0};  // Offset of the interaction point from the model in meters on the X,Y,Z axis.
+	};
+	class ACE_IED_V2_place_CUP: ACE_IED_V1_place_CUP {
+		model = "\CUP\Weapons\CUP_Weapons_Put\CUP_IED_V2.p3d";
+	};
+	class ACE_IED_V3_place_CUP: ACE_IED_V1_place_CUP {
+		model = "\CUP\Weapons\CUP_Weapons_Put\CUP_IED_V3.p3d";
+	};
+	class ACE_IED_V4_place_CUP: ACE_IED_V1_place_CUP {
+		model = "\CUP\Weapons\CUP_Weapons_Put\CUP_IED_V4.p3d";
+	};
 };
