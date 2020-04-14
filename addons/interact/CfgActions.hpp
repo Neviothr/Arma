1d9848e2f3062bf4b2d98a825ce8de9a0e682ed2
Neviothr <Neviothr@users.noreply.github.com>
2020-04-14 15:47:41 +0300

    * interact - CfgActions tabs -> spaces

--- addons/interact/CfgActions.hpp
+++ addons/interact/CfgActions.hpp
@@ -1,18 +1,18 @@
 class CfgActions {
     class None;
     class Heal: None {
-		text = "<t color='#E60000'>Treat at %1</t>";
-	};
-	class HealSoldier: None {
-		text = "<t color='#E60000'>Treat %1</t>";
-	};
+        text = "<t color='#E60000'>Treat at %1</t>";
+    };
+    class HealSoldier: None {
+        text = "<t color='#E60000'>Treat %1</t>";
+    };
     class FirstAid: None {
-		text = "<t color='#E60000'>First Aid</t>";
-	};
+        text = "<t color='#E60000'>First Aid</t>";
+    };
     class GetOut: None
-	{
-		text = "<t color='#ffa500'>Get Out</t>";
-	};
+    {
+        text = "<t color='#ffa500'>Get Out</t>";
+    };
     class HealSoldierSelf: None {
         text = "<t color='#E60000'>Treat yourself</t>";
     };
