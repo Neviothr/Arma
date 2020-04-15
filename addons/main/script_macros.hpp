dced7c139bc2db1f8bbadc0a23b48196ab387c64
Neviothr <Neviothr@users.noreply.github.com>
2020-04-15 15:11:41 +0300

    * main - remove double and unnecessary definitions

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -10,12 +10,4 @@
     #define PREP(fncName) [QPATHTOF(functions\DOUBLES(fnc,fncName).sqf), QFUNC(fncName)] call CBA_fnc_compileFunction
 #endif
 
-#ifdef DISABLE_COMPILE_CACHE
-    #undef PREP
-    #define PREP(fncName) DFUNC(fncName) = compile preprocessFileLineNumbers QPATHTOF(functions\DOUBLES(fnc,fncName).sqf)
-#else
-    #undef PREP
-    #define PREP(fncName) [QPATHTOF(functions\DOUBLES(fnc,fncName).sqf), QFUNC(fncName)] call CBA_fnc_compileFunction
-#endif
-
 #include "script_debug.hpp"
