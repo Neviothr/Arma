4af51ecac12da0583a0a39d4f303d42ac143fb7d
Neviothr <Neviothr@users.noreply.github.com>
2020-04-23 16:45:20 +0300

    Update RscDisplaySingleMission.hpp

--- addons/mission_framework/RscDisplaySingleMission.hpp
+++ addons/mission_framework/RscDisplaySingleMission.hpp
@@ -1,11 +1,11 @@
 class RscStandardDisplay;
 class RscDisplaySingleMission: RscStandardDisplay {
-	class controls {
+    class controls {
         class GVAR(mapName): RscText {
-			sizeEx = "0.048";
-			x = "SafeZoneX + (1200 / 1920) * SafeZoneW";
+            sizeEx = "0.048";
+            x = "SafeZoneX + (1200 / 1920) * SafeZoneW";
             y = "SafeZoneY + (44 / 1080) * SafeZoneH";
             text = "(Map name)";
         };
-	};
+    };
 };
