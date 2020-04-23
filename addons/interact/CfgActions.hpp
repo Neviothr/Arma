c4d994b2f89006e00eca153056e6501993660d89
Neviothr <Neviothr@users.noreply.github.com>
2020-04-23 16:50:52 +0300

    Update CfgActions.hpp

--- addons/interact/CfgActions.hpp
+++ addons/interact/CfgActions.hpp
@@ -9,27 +9,25 @@ class CfgActions {
     class FirstAid: None {
         text = "<t color='#E60000'>First Aid</t>";
     };
-    class GetOut: None
-    {
+    class GetOut: None {
         text = "<t color='#ffa500'>Get Out</t>";
     };
     class HealSoldierSelf: None {
         text = "<t color='#E60000'>Treat yourself</t>";
     };
     class GetInDriver: None {
-		text = "<t color='#ffa500'>Get in %1 as Driver</t>";
-	};
-	class GetInPilot: None {
-		text = "<t color='#ffa500'>Get in %1 as Pilot</t>";
-	};
-	class GetInGunner: None {
-		text = "<t color='#ffa500'>Get in %1 as gunner</t>";
-
-	};
-	class GetInCargo: None {
-		text = "<t color='#ffa500'>Get in %1 Ride in back</t>";
+        text = "<t color='#ffa500'>Get in %1 as Driver</t>";
     };
-		class GetInTurret: None {
-		text = "<t color='#ffa500'>Get in %1 as %2</t>";
-	};
+    class GetInPilot: None {
+        text = "<t color='#ffa500'>Get in %1 as Pilot</t>";
+    };
+    class GetInGunner: None {
+        text = "<t color='#ffa500'>Get in %1 as gunner</t>";
+    };
+    class GetInCargo: None {
+        text = "<t color='#ffa500'>Get in %1 Ride in back</t>";
+    };
+    class GetInTurret: None {
+        text = "<t color='#ffa500'>Get in %1 as %2</t>";
+    };
 };
