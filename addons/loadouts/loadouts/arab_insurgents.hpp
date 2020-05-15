7f0f204e48dd45c49c5cf6413ed515e0a5ea8b86
Neviothr <Neviothr@users.noreply.github.com>
2020-05-15 22:04:22 +0300

    * modfity arab_insurgets' vest, uniform and headgear
    * add arvn_1966
    * add aus_sf_ausmc
    * add australian_army_1970
    * add bundeswehr_1983
    * add bundeswehr_flecktarn
    * add bundeswehr_flecktarn_alt

--- addons/loadouts/loadouts/arab_insurgents.hpp
+++ addons/loadouts/loadouts/arab_insurgents.hpp
@@ -1,7 +1,7 @@
-class GVAR(rifleman) {
-    uniformClass = "U_C_Poor_1";
-    backpack = "B_FieldPack_blk";
-    linkedItems[] = {"CUP_I_B_PMC_Unit_13", "V_TacVest_blk", "H_ShemagOpen_tan", "ItemCompass", "ItemWatch", "ItemRadio"};
+class GVAR(r) {
+    uniformClass = "CUP_I_B_PMC_Unit_13";
+    backpack = "";
+    linkedItems[] = {"V_TacVest_blk", "H_Shemag_olive", "ItemCompass", "ItemWatch", "ItemRadio"};
     weapons[] = {"rhs_weap_akm"};
     magazines[] = {
         LIST_7("rhs_30Rnd_762x39mm"),
@@ -12,9 +12,9 @@ class GVAR(rifleman) {
 };
 
 class GVAR(g) {
-    uniformClass = "U_I_C_Soldier_Bandit_1_F";
-    backpack = "B_FieldPack_blk";
-    linkedItems[] = {"CUP_U_B_BDUv2_roll2_dirty_desert", "V_I_G_resistanceLeader_F", "CUP_H_PMC_Cap_Back_Grey", "ItemCompass", "ItemWatch", "ItemRadio"};
+    uniformClass = "CUP_I_B_PMC_Unit_15";
+    backpack = "";
+    linkedItems[] = {"V_TacVest_brn", "H_ShemagOpen_tan", "ItemCompass", "ItemWatch", "ItemRadio"};
     weapons[] = {"rhs_weap_akm_gp25"};
     magazines[] = {
         LIST_6("rhs_30Rnd_762x39mm"),
@@ -26,9 +26,9 @@ class GVAR(g) {
 };
 
 class GVAR(rat) {
-    uniformClass = "U_I_L_Uniform_01_tshirt_sport_F";
+    uniformClass = "CUP_I_B_PMC_Unit_16";
     backpack = "rhs_rpg";
-    linkedItems[] = {"usm_headwrap_tan", "V_TacVest_brn", "CUP_H_TKI_Lungee_05", "ItemCompass", "ItemWatch", "ItemRadio"};
+    linkedItems[] = {"V_I_G_resistanceLeader_F", "H_ShemagOpen_khk", "ItemCompass", "ItemWatch", "ItemRadio"};
     weapons[] = {"rhs_weap_akmn", "rhs_weap_rpg7"};
     magazines[] = {
         LIST_7("rhs_30Rnd_762x39mm"),
@@ -40,12 +40,12 @@ class GVAR(rat) {
 };
 
 class GVAR(ar) {
-    uniformClass = "CUP_I_B_PARA_Unit_10";
-    backpack = "bear_sidor_tan";
-    linkedItems[] = {"usm_headwrap_tgrstp", "rhsgref_chicom", "CUP_H_TKI_SkullCap_05", "ItemCompass", "ItemWatch", "ItemRadio"};
+    uniformClass = "CUP_I_B_PMC_Unit_35";
+    backpack = "";
+    linkedItems[] = {"V_TacVest_oli", "rhssaf_helmet_m97_black_nocamo", "ItemCompass", "ItemWatch", "ItemRadio"};
     weapons[] = {"CUP_arifle_RPK74"};
     magazines[] = {
-         LIST_10("CUP_40Rnd_TE4_LRT4_Green_Tracer_762x39_RPK_M"),
+        LIST_10("CUP_40Rnd_TE4_LRT4_Green_Tracer_762x39_RPK_M"),
         "rhs_mag_f1",
         "rhs_mag_rdg2_white"
     };
